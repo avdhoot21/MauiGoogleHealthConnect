d2a9b5446ddaac94049e587fdf87ec444af45992
Avdhoot Ambal <avdhoot@mindzillion.com>
2025-01-04 18:21:03 +0530

    Add project files.

---
+++ MauiHealthConnect/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiHealthConnect
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
